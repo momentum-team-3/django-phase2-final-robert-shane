4e622184de3c6a39bc7ed1ac6bef174631dabb0d
shaneneiman <shane.neiman@gmail.com>
2020-09-01 15:13:21 -0400

    list_snippet changed to display snippets

--- codesnippet/snippets/urls.py
+++ codesnippet/snippets/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 path("list/", views.list_snippet.as_view(), name="list_snippet"),
-path("view/", views.view_snippet.as_view(), name="view_snippet"),
+path("view/<int:pk>", views.view_snippet.as_view(), name="view_snippet"),
 path("add/", views.add_snippet.as_view(), name="add_snippet"),
 
 
